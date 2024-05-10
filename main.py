c6177600b964c13d8e909289c8559bf9f55a5324
suditn <43325914+suditn@users.noreply.github.com>
2024-05-10 21:26:54 +0300

    Добавил сохранение pdf

--- main.py
+++ main.py
@@ -16,11 +16,14 @@ driver = webdriver.Edge()
 url = 'https://www.vishay.com/en/inductors/'
 save_patch = str(Path(__file__).parent.resolve())
 img_small_save_patch = save_patch+"\\image\\small_inductors\\"
+datash_save_patch = save_patch+"\\Datasheet\\"
+headers = {'User-Agent': "scrapping_script/1.0"}
 
 def get_web(u):
     driver.get(u)
     print(save_patch)
     Path(img_small_save_patch).mkdir(parents=True, exist_ok=True)
+    Path(datash_save_patch).mkdir(parents=True, exist_ok=True)
     option2 = driver.find_element('xpath', '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div/div/div/div[3]/div[2]/label/select/option[1]')
     max_ent = driver.find_element('xpath', '/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div/div/div/div[3]/div[1]/div').text
 # //*[@id="Table_vshGenTblPaginationInfo__29nYK"]/div/text()[6]
@@ -30,7 +33,8 @@ def get_web(u):
     time.sleep(3)
     webso = driver.page_source
     table = []
-    df = pd.DataFrame()
+#    df = pd.DataFrame()
+#    df_datasheet = pd.DataFrame(['Datasheet'])
 #    df = pd.read_html(StringIO(webso))
 #    print(df[3])
 
@@ -41,6 +45,8 @@ def get_web(u):
     img_alt = []
     img_pr = ''
     img_pr2 =''
+    datash_src= []
+    datash_pr = ''
     columns = [i.get_text(strip=True) for i in table.find_all("th")]
     data = []
 
@@ -48,7 +54,10 @@ def get_web(u):
         data.append([td.get_text(strip=True) for td in tr.find_all("td")])
 
     df = pd.DataFrame(data, columns=columns, )
+    i = 0
     for im in img:
+
+        ser = df['Series▲▼'][i]
         if (im['src'].split('/')[-2]=='pt-small'):
             img_src.append(img_small_save_patch+im['alt']+'.png')
             img_alt.append(im['alt'])
@@ -60,16 +69,51 @@ def get_web(u):
                     with open(img_small_save_patch+im['alt']+'.png', 'wb') as out_file:
                         shutil.copyfileobj(img_requ.raw, out_file)
                     print('файл '+im['alt']+'.png создан')
-                del img_requ
+
                 img_pr = im['src']
-#            print(img_pr)
-            img_pr2 = im['alt']
+                del img_requ
+            datash_src.append(datash_save_patch + ser + '\\' + ser + '.pdf')
+            i += 1
+
+
+
+        if(im['alt'] != "Datasheet" and datash_pr != ser):
+            datash_requ = requests.get('https://www.vishay.com/doc?'+im['alt'],headers=headers, stream = True)
+            Path(datash_save_patch+'\\'+ser).mkdir(parents=True, exist_ok=True)
+            print(i)
+            print(datash_requ.url)
+            if os.path.exists(datash_save_patch + '\\' + ser+'\\'+ser+'.pdf'):
+                print('файл '+ser+' существует')
+            else:
+                with open(datash_save_patch + ser + '\\' + ser+'.pdf', 'wb') as out_file:
+                    out_file.write(datash_requ.content)
+                print('файл ' + ser + ' создан')
+
+            del datash_requ
+
+            datash_pr = ser
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     writer = pd.ExcelWriter(save_patch + '/' + url.split('/')[-2] + '.xlsx', engine='xlsxwriter')
     df_img = pd.DataFrame(img_src)
+    df_datasheet = pd.DataFrame(datash_src, columns=['Datasheet'])
     df['Product Image'] = df_img
 
-    df.to_excel(writer, index=False, sheet_name='Inductors')
+    df.join(df_datasheet).to_excel(writer, index=False, sheet_name='Inductors')
 
     worksheet = writer.sheets['Inductors']
     worksheet.autofit()
